a641c76b783758ae79adc8400a02f9617664775a
Maël Billaud <92854452+maelBillaud@users.noreply.github.com>
2022-03-10 10:50:58 +0100

    Add alter_tables, insert_tables and update create_tables

---
+++ Code/insert_tables.sql
@@ -0,0 +1,14 @@
+prompt *************************************************************
+prompt ******************** DELETE TABLE *****************************
+prompt *************************************************************
+
+DELETE FROM Restaurant;
+DELETE FROM Client;
+DELETE FROM Coursier;
+DELETE FROM Course;
+DELETE FROM Commande;
+DELETE FROM Specialite;
+
+prompt *************************************************************
+prompt ******************** INSERT TUPLES **************************
+prompt *************************************************************
