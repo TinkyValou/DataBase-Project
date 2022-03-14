ca07083506fd3cade20838d7db73cea05aefa008
Mael BILLAUD <billaud.mael@gmail.com>
2022-03-15 00:10:43 +0100

    Create index_tables.sql

---
+++ Code/index_tables.sql
@@ -0,0 +1,5 @@
+prompt *************************************************************
+prompt ***************** Indexe TABLE Commande *********************
+prompt *************************************************************
+
+CREATE INDEX IX_nom_prix_menu ON Commande (nom_menu, prix_menu);
