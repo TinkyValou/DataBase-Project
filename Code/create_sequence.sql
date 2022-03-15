b1bdfb6b559e78622f3b063173379dd676020eea
Maël Billaud <92854452+maelBillaud@users.noreply.github.com>
2022-03-15 20:59:36 +0100

    Update create_sequence and insert_tables

--- Code/create_sequence.sql
+++ Code/create_sequence.sql
@@ -1,13 +1,23 @@
-CREATE SEQUENCE Commande.number_of_commande
-START WITH 1
+prompt *************************************************************
+prompt ******************* DELETE SEQUENCE *************************
+prompt *************************************************************
+
+DROP SEQUENCE number_of_commande;
+
+prompt *************************************************************
+prompt ******************* CREATE SEQUENCE *************************
+prompt *************************************************************
+
+CREATE SEQUENCE number_of_commande
+START WITH 0
 INCREMENT BY 1
-NOMAXVALUES 
+NOMAXVALUE
+MINVALUE 0
 NOCYCLE
 NOCACHE ;
 
 -- Initialisation de la séquence
-SELECT Commande.number_of_commande.nextval FROM dual;
-
+SELECT number_of_commande.nextval FROM dual;
 
 -- Commande pour obtenir le nombre courrant de commande.
--- SELECT Commande.number_of_commande.currval FROM dual; 
+-- SELECT number_of_commande.currval FROM dual; 
