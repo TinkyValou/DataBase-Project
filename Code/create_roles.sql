0bc0fb87b5e99c54ea6685f52f8882bcd58196ba
Maël Billaud <92854452+maelBillaud@users.noreply.github.com>
2022-03-17 10:43:33 +0100

    Update roles

--- Code/create_roles.sql
+++ Code/create_roles.sql
@@ -1,3 +1,79 @@
+-- create_roles pour pc FAC
+
+prompt *************************************************************
+prompt ********************** DROP ROLES ***************************
+prompt *************************************************************
+
+DROP ROLE DB_admin;
+DROP ROLE CEO;
+DROP ROLE Coursier;
+DROP ROLE Restaurateur;
+DROP ROLE Client;
+
+prompt *************************************************************
+prompt ********************* CREATE ROLES **************************
+prompt *************************************************************
+
+CREATE ROLE DB_admin IDENTIFIED BY DB_admin;
+CREATE ROLE CEO IDENTIFIED BY CEO;
+CREATE ROLE Coursier;
+CREATE ROLE Restaurateur;
+CREATE ROLE Client;
+
+prompt *************************************************************
+prompt ****************** PRIVILEGE TO CLIENT **********************
+prompt *************************************************************
+
+GRANT SELECT ON Restaurant TO Client;
+GRANT SELECT ON Specialite TO Client;
+GRANT INSERT, UPDATE ON Client TO Client;
+GRANT INSERT, UPDATE ON Commande TO Client;
+
+
+prompt *************************************************************
+prompt *************** PRIVILEGE TO RESTAURATEUR *******************
+prompt *************************************************************
+
+GRANT SELECT ON Commande TO Restaurateur;
+GRANT INSERT, UPDATE ON Specialite TO Restaurateur;
+GRANT INSERT, UPDATE ON Restaurant TO Restaurateur;
+
+prompt *************************************************************
+prompt ***************** PRIVILEGE TO COURSIER *********************
+prompt *************************************************************
+
+GRANT SELECT ON CLient TO Coursier;
+GRANT SELECT ON Commande TO Coursier;
+GRANT INSERT, UPDATE ON Coursier TO Coursier;
+
+prompt *************************************************************
+prompt ******************* PRIVILEGE TO CEO ************************
+prompt *************************************************************
+
+GRANT SELECT, INSERT, UPDATE ON Specialite TO CEO;
+GRANT SELECT, INSERT, UPDATE ON Restaurant TO CEO;
+GRANT SELECT, INSERT, UPDATE ON Client TO CEO;
+GRANT SELECT, INSERT, UPDATE ON Coursier TO CEO;
+GRANT SELECT, INSERT, UPDATE ON Course TO CEO;
+GRANT SELECT, INSERT, UPDATE ON Commande TO CEO;
+
+prompt *************************************************************
+prompt ***************** PRIVILEGE TO DB_admin *********************
+prompt *************************************************************
+
+GRANT ALL PRIVILEGES TO DB_admin;
+
+prompt *************************************************************
+prompt ********************* ROLE TO USERS *************************
+prompt *************************************************************
+
+GRANT Coursier TO L3_2;
+GRANT Client TO L3_6;
+GRANT DB_admin TO L3_7;
+GRANT CEO TO L3_11;
+
+-- create_roles pour pc Maël
+/*
 prompt *************************************************************
 prompt ********************** DROP ROLES ***************************
 prompt *************************************************************
@@ -85,3 +161,4 @@ GRANT C##DB_admin TO C##L3_7;
 
 CREATE USER C##L3_11;
 GRANT C##CEO TO C##L3_11;
+*/
